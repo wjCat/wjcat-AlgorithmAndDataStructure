4b905b7211c7857e0a3e1a91d48476f9dfde779f
wjcat <396898146@qq.com>
2019-05-18 10:28:18 +0800

    数组

--- src/com/wjcat/learn/datastructure/Arrays/Main.java
+++ src/com/wjcat/learn/datastructure/Arrays/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList arr = new ArrayList(20);
-        for(int i = 0 ; i < 10 ; i ++)
+        for(int i = 0 ; i < 40 ; i ++)
             arr.addLast(i);
         System.out.println(arr);
 
@@ -26,6 +26,11 @@ public class Main {
 
         arr.removeFirst();
         System.out.println(arr);
+
+        for(int i = 0 ; i < 39 ; i ++){
+            arr.removeFirst();
+            System.out.println(arr);
+        }
     }
 
 }
